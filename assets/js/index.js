f384ab65d48656713724f3af3af700bb9a8a5dad
matiasllanos <m.llanos.vidal@hotmail.com>
2024-03-31 05:37:23 -0300

    commit 6

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+
+function scrollToSection(id) {
+    const section = document.getElementById(id);
+    if (section) {
+        window.scrollTo({
+            top: section.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+}
+
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        scrollToSection(targetId);
+    });
+});
